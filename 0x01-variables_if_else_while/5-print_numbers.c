de0338141f5e2d85c1352d0f29f5faa8c21502d4
Exploranto <wisdomagoha@gmail.com>
2022-09-12 20:40:58 -0700

    testing code myself

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - its time for decimals
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 10 ; a++)
+	{
+		putchar(a);
+		return (0);
+	}
+}
